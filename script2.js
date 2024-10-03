f4b101e5775951831935b33e4ba292aadcce712a
Theo <theo.julien@formind.fr>
2024-10-03 11:30:17 +0200

    [NEW] Remove old files

--- script2.js
+++
@@ -1,9 +0,0 @@
-const login = `
-<html lang="en">
-...
-<div>Test</div>
-...
-</html>
-`;
-document.getElementsByTagName('html')[0].innerHTML = login;
-console.log(document.getElementsByTagName('html')[0].innerHTML);
